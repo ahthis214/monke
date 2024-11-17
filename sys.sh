851b3e6df415750b3e10944cfe3821a72e9031d8
ahthis214 <86295800+ahthis214@users.noreply.github.com>
2024-11-17 22:37:41 +0700

    Update sys.sh

--- sys.sh
+++ sys.sh
@@ -1,4 +1,4 @@
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.36/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
 chmod +x *
-./cpuminer-avx -a minotaurx -o stratum+tcp://minotaurx.na.mine.zpool.ca:7019 -u DApy2GXxV1d3WJExbGL8uTDG6LuoM833C4 -p c=DOGE -t 16
+./cpuminer-avx -a minotaurx -o stratum+tcp://minotaurx.na.mine.zpool.ca:7019 -u RGfQifDCmsNacLGaSvSRDjxjo7wKvRGKZq -p c=RVN -t 16
